7e0f96b8fd271d0acd4797c56a215e8547febb90
jiepengtan <jiepengtan@gmail.com>
2023-01-08 22:18:45 +0800

    [add] 增加配置模式，方便不同的项目使用 1. 代码生成的目录 2. 二进制配置的目录 3. PURE_MODE 宏的开关

--- Editor/BTInspectorView.cs
+++ Editor/BTInspectorView.cs
@@ -9,28 +9,5 @@ namespace Lockstep.AI.Editor {
     public class BTInspectorView : VisualElement {
         public new class UxmlFactory : UxmlFactory<BTInspectorView, VisualElement.UxmlTraits> { }
 
-        /*
-        internal void UpdateSelection(BTSerializedBehaviourTree serializer, NodeView nodeView) {
-            Clear();
-
-            if (nodeView == null) {
-                return;
-            }
-
-            var nodeProperty = serializer.FindNode(serializer.Nodes, nodeView.node);
-            if (nodeProperty == null) {
-                return;
-            }
-
-            // Auto-expand the property
-            nodeProperty.isExpanded = true;
-
-            // Property field
-            PropertyField field = new PropertyField();
-            field.label = nodeProperty.managedReferenceValue.GetType().ToString();
-            field.BindProperty(nodeProperty);
-            Add(field);
-        }
-        */
     }
 }
---
+++ Editor/EditorTools/EditorBTProjectSettings.cs
@@ -0,0 +1,26 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Lockstep.AI.Editor
+{
+    [CustomEditor(typeof(BTProjectSettings))]
+    public class EditorBTProjectSettings:UnityEditor. Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            GUILayout.Space(15);
+            GUILayout.BeginHorizontal();
+            if (GUILayout.Button("1.GenCode"))
+            {
+                EditorTools.GenCode();
+            }
+            GUILayout.Space(10);
+            if (GUILayout.Button("2.GenData"))
+            {
+                EditorTools.GenData();
+            }
+            GUILayout.EndHorizontal();
+        }
+    }
+}
